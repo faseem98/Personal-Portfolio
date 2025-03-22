298980a54b89789625ab8269617f360ff3c82df2
Faseem Ahmed <fantasticfas98@gmail.com>
2025-03-22 13:52:02 +0530

    resume changes

--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -10,7 +10,7 @@ import React, { useState, useEffect } from "react";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/faseem98/Personal-Portfolio/main/src/assets/FASEEM.pdf";
+  "https://raw.githubusercontent.com/faseem98/Personal-Portfolio/main/src/assets/FaseemResume.pdf";
 
 
 function Resume() {
